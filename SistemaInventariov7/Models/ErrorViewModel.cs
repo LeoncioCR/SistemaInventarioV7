01b7d185a8590fab4ce6c5ae7dff6d0bdb30a039
LeoncioCR <leocuevaruiz@gmail.com>
2023-08-27 15:18:59 -0500

    Crear Proyecto

---
+++ SistemaInventariov7/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaInventariov7.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
